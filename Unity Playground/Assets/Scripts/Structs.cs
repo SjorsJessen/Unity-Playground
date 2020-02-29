10db2b9f97d431d15857c4b530f3f579a8c22084
Sjors Jessen <sjors.jessen@gmail.com>
2020-02-29 20:45:42 +0100

    Added Struct component definition

---
+++ Unity Playground/Assets/Scripts/Structs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structs : MonoBehaviour
+{
+    private void Start()
+    {
+        Car mercedes;
+        mercedes.Brand = "Mercedes";
+        mercedes.Model = "AMG";
+        mercedes.CarColor = Color.black;
+        mercedes.Cost = 30000f;
+        
+        Debug.Log(mercedes.Brand + mercedes.Model + mercedes.CarColor + mercedes.Cost);
+    }
+}
+
+public struct Car
+{
+    public string Brand;
+    public string Model;
+    public Color CarColor;
+    public float Cost;
+}
